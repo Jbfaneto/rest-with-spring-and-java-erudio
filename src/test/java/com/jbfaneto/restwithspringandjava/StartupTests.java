957eab535dfb97c2e8954563d46d07a653b802cc
Jbfaneto <jbfaneto@gmail.com>
2023-10-30 20:42:44 -0300

    First commit

---
+++ src/test/java/com/jbfaneto/restwithspringandjava/StartupTests.java
@@ -0,0 +1,13 @@
+package com.jbfaneto.restwithspringandjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
